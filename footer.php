f38debccf63739db0fd4de6ae749e07036970105
Hunter Cox <hsc@Hunters-MBP.hsd1.in.comcast.net>
2021-01-30 16:24:40 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p><?php bloginfo('name'); ?></p>
+</footer>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+    <main id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+        <h2><?php the_title(); ?></h2>
+        <div class="inner-content">
+          <?php the_content(); ?>
+        </div><!-- /.inner-content -->
+      </article>
+      <?php endwhile; ?>
+        <h4>Other stuff happening after the loop...</h4>
+			<?php else : ?>
+        <p>There is nothing in the loop.</p>
+      <?php endif; ?>
+    </main>
+
+<?php get_footer(); ?>
